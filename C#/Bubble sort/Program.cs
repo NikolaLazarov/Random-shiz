f4db569ce4b63793503c8b8baacd27d4f528c8a1
Nikola Lazarov <hazegamingbg@gmail.com>
2022-05-29 13:24:00 +0300

    ENFS and Bubble sort
    If wishes were fishes we would cast nests.
    
    -Gurney Halleck

---
+++ C#/Bubble sort/Program.cs
@@ -0,0 +1,33 @@
+﻿int mid;
+int p = 0;
+Random rnd = new Random();
+int[] arrNums = new int[7];
+Console.WriteLine("The original array: ");
+for (int i = 0; i < arrNums.Length; i++)
+{
+    arrNums[i] = rnd.Next(1, 9);
+    Console.Write(arrNums[i] + " ");
+}
+Console.WriteLine();
+for (int k = 0; k < arrNums.Length; k++)
+{
+    for (int i = 0; i < arrNums.Length - 1; i++)
+    {
+        if (arrNums[i] > arrNums[i + 1])
+        {
+            mid = arrNums[i];
+            arrNums[i] = arrNums[i + 1];
+            arrNums[i + 1] = mid;
+            p++;
+        }
+    }
+    if (p == 0)
+    {
+        break;
+    }
+}
+Console.WriteLine("\nThe array sorted via Bubble sort:");
+for (int i = 0; i < arrNums.Length; i++)
+{
+    Console.Write(arrNums[i] + " ");
+}
---
+++ C#/Extractnumfromstring/Program.cs
@@ -0,0 +1,19 @@
+﻿string t = "The sun is 400 times bigger then the moon and π is equal to 3.1415";
+string s = "";
+char[] nums = t.ToCharArray();
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] == '.' || nums[i] >= (char)48 && nums[i] <= (char)57)
+    {
+        s += nums[i];
+        if (nums[i] != nums[nums.Length - 1])
+        {
+            if (nums[i + 1] != '/' && nums[i + 1] < (char)46 || nums[i + 1] > (char)57)
+            {
+                s += " ";
+            }
+        }
+    }
+}
+Console.WriteLine("The string:\n" + t);
+Console.WriteLine("\nThe extracted numbers:\n" + s);
