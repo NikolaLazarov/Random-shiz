c5886fac9df12c2b65b3213e3d6c8bc68e60ee35
Nikola Lazarov <hazegamingbg@gmail.com>
2022-07-06 20:48:26 +0300

    Sorting algorithm: Selection Sort
    It is an easy algorithm once you understand it which takes 5-10 minutes

---
+++ C#/Selection sort/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(10);
+            Write(" " + array[i]);
+        }
+        WriteLine();
+        Selection_Sort(array);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Write(" " + array[i]);
+        }
+        WriteLine();
+
+        /*selection sort*/
+        void Selection_Sort(int[] array)
+        {
+            //fist int that runs the length of the array - 1 because of j = i + 1
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                //creating the min var which holds the index of i
+                int min = i;
+                //in the second for loop we find the index of the lowest element in the array
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+                //here we swap the places of element of index i and the element of index min
+                int temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+        }
+
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
